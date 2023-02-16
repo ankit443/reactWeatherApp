b84c9e9dd390acedf6ffe93f16c3a91ffc704cd4
Ankit Deb <deb.ankit.work@gmail.com>
2023-02-16 21:42:22 +0530

    worked on figuring out the detail structure for weather display

--- src/components/currentWeather/currentWeather.js
+++ src/components/currentWeather/currentWeather.js
@@ -5,11 +5,40 @@ const CurrentWeather = () => {
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">Delhi</p>
-                <p className="weather-description">Sunny</p>
-            </div>
 
-            <img alt="weather" className="weatherIcon" src="icons/01d.png" />
+                <div>
+
+                    <p className="city">Delhi</p>
+                    <p className="weatherDescription">Sunny</p>
+
+                </div>
+                <img alt="weather" className="weatherIcon" src="icons/01d.png" />
+            </div>
+            <div className="bottom">
+                <p className="temperature">18°C</p>
+                <div className="details">
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Details</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Feels like</span>
+                        <span className="parameterValue">22°C</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Wind</span>
+                        <span className="parameterValue">2 m/s</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Humidity</span>
+                        <span className="parameterValue">15 %</span>
+                    </div>
+                    <div className="parameterRow">
+                        <span className="parameterLabel">Pressure</span>
+                        <span className="parameterValue">15 hPpa</span>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 
