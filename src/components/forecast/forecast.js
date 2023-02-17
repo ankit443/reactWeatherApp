1b910697756218e28dbf9d5132e7f4ec4f88392c
Ankit Deb <deb.ankit.work@gmail.com>
2023-02-17 07:10:35 +0530

    setting up responsive values for forecasts

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,6 +1,13 @@
 import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
 
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 const Forecast = ({ data }) => {
+
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+    console.log(forecastDays) //Gotta delete it
 
     return (
         <>
@@ -8,10 +15,15 @@ const Forecast = ({ data }) => {
             <label className="title">Daily Forecast</label>
             <Accordion allowZeroExpanded>
                 {data.list.splice(0, 7).map((item, idx) => (
-                    <AccordionItem key ={idx}>
+                    <AccordionItem key={idx}>
                         <AccordionItemHeading>
                             <AccordionItemButton>
-                                Hello
+                                <div className="dailyItem">
+                                    <img alt="weather" className="iconSmall" src={`icons/${item.weather[0].icon}.png`} />
+                                    <label className="day">{forecastDays[idx]}</label>
+                                    <label className="description">{item.weather[0].description}</label>
+                                    <label className="minMax">{Math.round(item.main.temp_min)}°C / {Math.round(item.main.temp_max)}°C</label>
+                                </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
                         <AccordionItemPanel></AccordionItemPanel>
